28460cdca0d1bb54bbc0c2f38018badf97fde46b
Interianxx <al070687@uacam.mx>
2025-03-28 01:03:13 -0600

    Edicion de portada

--- src/components/HeroSection.jsx
+++ src/components/HeroSection.jsx
@@ -3,6 +3,8 @@ import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {faCartShopping, } from '@fortawesome/free-solid-svg-icons';
 import React from "react";
 import videoSrc from '../../public/videos/video-2.webm';
+import './HeroSection.css';
+import '../App.css'
 
 const HeroSection = () => {
 	return (
